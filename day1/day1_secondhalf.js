8b0d2b3ebc6adc1e83f0ce22448ae1c12f99b179
rookiemonkey <kevinroirigorbasina@pm.me>
2020-12-04 17:25:16 +0800

    answered a puzzle

--- day1/day1_secondhalf.js
+++ day1/day1_secondhalf.js
@@ -1,11 +1,3 @@
-/**
- * Using the above example again, the three entries that sum to 2020 
- * are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
- * 
- * In your expense report, what is the product of the three entries that sum to 2020?
- */
-
-
 const input = require('./input');
 const results = new Array();
 
--- day2/day2_secondhalf.js
+++ day2/day2_secondhalf.js
@@ -1,16 +1,3 @@
-/**
-
-The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
-
-Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
-
-Given the same example list from above:
-
-1-3 a: abcde is valid: position 1 contains a and position 3 does not.
-1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
-2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
-
- */
 const input = require('./input')
 
 // used REGEX to split the string
